5c3af92606b6e221dd0c77d40b63964614a8fee8
suraj sachan <100021123+Surajsachan29889@users.noreply.github.com>
2023-11-11 01:56:23 +0530

    dynamic import addition

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,36 @@
-import Hero from './components/hero'
-import Navbar from './components/navbar'
-import CenterModeSlickCarousel from './components/slider';
-import Midsec1 from './components/midsec-1';
-import Midsec2 from './components/midsec-2';
-import Footer1 from './components/footer-1';
-import Footer2 from './components/footer-2';
+import { lazy, Suspense} from 'react';
+import GridLoader from 'react-spinners/GridLoader';
+// import Navbar from './components/navbar';
+// import Hero from './components/hero';
+
+// Use React.lazy for dynamic imports
+const CenterModeSlickCarousel = lazy(() => import('./components/slider'));
+const Midsec1 = lazy(() => import('./components/midsec-1'));
+const Hero = lazy(() => import('./components/hero'));
+const Navbar = lazy(() => import('./components/navbar'));
+const Midsec2 = lazy(() => import('./components/midsec-2'));
+const Footer1 = lazy(() => import('./components/footer-1'));
+const Footer2 = lazy(() => import('./components/footer-2'));
+
 
 function App() {
-  
   return (
     <div>
-      <Navbar/>
-      <Hero/>
-      <CenterModeSlickCarousel/>
-      <Midsec1/>
-      <Midsec2/>
-      <Footer1/>
-      <Footer2/>
+      <Suspense fallback={<div className='absolute top-[25%] left-[40%] loader '><GridLoader
+          size={100}  
+          color='#00CC99'
+      /></div>}>
+      <Navbar />
+      <Hero />
+      {/* Wrap lazy-loaded components with Suspense */}
+        <CenterModeSlickCarousel />
+        <Midsec1 />
+        <Midsec2 />
+        <Footer1 />
+        <Footer2 />
+      </Suspense>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Ensure that assets are emitted to the root path
+    assetsDir: '',
+  },
 })
